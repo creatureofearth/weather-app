731e8e08ca181934748928ec8717c9411f86e5b8
MujeebaP <mujeeba03@hotmail.com>
2023-03-19 17:14:53 +0000

    edited the map+ added dependencies

--- src/routes/map/index.js
+++ src/routes/map/index.js
@@ -1,29 +1,41 @@
-import { useEffect } from 'preact/hooks';
+import { h, Component } from 'preact';
 import style from './style.css'; 
- 
 
+class Map extends Component {
+  constructor(props) {
+    super(props); 
 
-const Map = () => { 
-	  useEffect(() => {     
-		
-const script = document.createElement('script');     
-script.src = 'https://api.mapbox.com/mapbox-gl-js/v2.5.0/mapbox-gl.js';  
+    this.mapRef = null; // reference to the DOM element
+  }
 
-script.onload = () => {       
-	mapboxgl.accessToken = 'pk.eyJ1IjoiYnJpbmRhLTA5IiwiYSI6ImNsZjcydGR0dTFzZXYzcHIwZ3Ixdmo3YjEifQ.wZBtE8HK4-B-_LI9zYTJ3Q';       
-	new mapboxgl.Map({         
-		container: 'map-container',         
-		style: 'mapbox://styles/mapbox/streets-v11'
-	});     
-	};     document.head.appendChild(script);   }, []);   
-	
+  componentDidMount() {
+    mapboxgl.accessToken = 'pk.eyJ1IjoibXVqZWViYSIsImEiOiJjbGZmZHM5MTkwdm15NDBuemM3dzlycmJvIn0.NHuXP35hgr70TT3p1Z27Vw';
 
-	return ( 
-		
-		
-		<div class={style.main}>       
-	      <div id="map-container" class={style.map}></div>     
-		  </div>   ); 
-		  }; 	
-	
-    export default Map;
+    const map = new mapboxgl.Map ({ 
+      container: this.mapRef, // container id
+      style: 'mapbox://styles/mapbox/streets-v12', // stylesheet location
+      center: [-0.04053,51.52441], // starting position [lng, lat] at Mile End
+      zoom: 13
+    });
+
+    map.addControl(
+      new MapboxDirections({
+        accessToken: mapboxgl.accessToken, // Set the access token
+		profile: 'mapbox/cycling' //Cycling directions open first.
+      }),
+      'top-left'
+    );
+  }
+
+render() {
+    return (
+      <div>
+        <div ref={(ref) => (this.mapRef = ref)} // reference to the DOM element 
+		style={{ position: 'absolute', top: 0, bottom: 0, width: '100%' }} // style the map container
+		 />
+      </div>
+    );
+  }
+}
+
+export default Map;
